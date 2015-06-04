4e2b09d46dc77f8988a72deca5489b360b84777c
espiler <monkeysrawesome@gmail.com>
2015-06-04 11:06:51 -0700

    adds basic child functions and tests

--- familyTree.js
+++ familyTree.js
@@ -4,3 +4,25 @@ function FamilyTree(name, parent) {
 	this.parent = parent;
 	return this;
 }
+
+FamilyTree.prototype.addChildren = function(children) {
+	var _this = this
+	var args = Array.prototype.slice.call(arguments);
+	args.forEach(function(child){ 
+		_this.children.push(new FamilyTree(child, _this));	
+	});
+	return this;
+}
+
+FamilyTree.prototype.addChild = function(child) {
+	child = new FamilyTree(child, this);
+	this.children.push(child);
+	return child;
+}
+
+FamilyTree.prototype.getChild = function(child) {
+	for (var i = 0; i < this.children.length; i++) {
+		if (this.children[i].name == child) { return this.children[i]; }
+	};
+	return null;
+}
--- test/test.js
+++ test/test.js
@@ -1,4 +1,31 @@
 describe("Family Tree", function() {
 
+  var nancy, kevin;
+
+  beforeEach(function() {
+    nancy = new FamilyTree('Nancy').addChildren('Adam','Jill','Carl');
+    nancy.getChild('Carl').addChildren('Catherine','Joseph');
+    kevin = nancy.getChild('Jill').addChild('Kevin');
+    kevin.addChildren('Samuel','George','James','Aaron');
+    kevin.getChild('James').addChild('Mary');
+    kevin.getChild('George').addChildren('Patrick', 'Robert');
+  });
+  
+  it("should create new nodes with names", function(){
+    expect(nancy.name).to.equal('Nancy');
+  });
+
+  it("should add a child", function(){
+    nancy = new FamilyTree('Nancy');
+    nancy.addChild('Adam');
+    expect(nancy.children.length).to.equal(1);
+    expect(nancy.children[0].name).to.equal('Adam');
+  });  
+
+  it("should find a child", function(){
+    expect(nancy.getChild('Carl').name).to.equal('Carl');
+    expect(nancy.getChild('Betty')).to.equal(null);
+  })
+
 
 });
