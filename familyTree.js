494ebf176b8f54fd76ad994c6cad1f1754fc2c60
espiler <monkeysrawesome@gmail.com>
2015-06-04 10:53:54 -0700

    setup npm, test, js

---
+++ familyTree.js
@@ -0,0 +1,6 @@
+function FamilyTree(name, parent) {
+	this.children = [];
+	this.name = name;
+	this.parent = parent;
+	return this;
+}
---
+++ test/test.js
@@ -0,0 +1,4 @@
+describe("Family Tree", function() {
+
+
+});
